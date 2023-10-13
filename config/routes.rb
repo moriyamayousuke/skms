8b8626509609dd7a46e8f5b2cf51d01d90cfd32f
20211414 <20211414@example.com>
2023-10-13 13:37:16 +0900

    5回目のコミット　home/topの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/top'
+  root to:'home#top'
   resources :loan_details
   resources :key_masters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20231011022834_create_key_masters.rb
+++ db/migrate/20231011022834_create_key_masters.rb
@@ -8,3 +8,4 @@ class CreateKeyMasters < ActiveRecord::Migration[6.1]
     end
   end
 end
+n
